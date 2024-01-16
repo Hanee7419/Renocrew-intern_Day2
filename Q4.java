483b8299adf949807cd2ff1d4a3e8df5c0d90d98
Hanee7419 <141395549+Hanee7419@users.noreply.github.com>
2024-01-16 16:49:03 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,10 @@
+public class Q4 {
+    public static void main(String[] args) {
+        float v = 12 ;
+        float u = 5 ;
+        float a = 2 ;
+        float s = 3 ;
+        float sc = (v*v - u*u)/(2*a*s);
+        System.out.println(sc);
+    }
+}
